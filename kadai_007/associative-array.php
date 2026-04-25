a17da1db5b82ae524d24c3e35efaef8a6ce22c14
a-takahashi615 <asu61510ka@gmail.com>
2026-04-26 00:21:59 +0900

    kadai_007のアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>連想配列</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 連想配列を作成
+    $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 連想配列を出力
+    print_r($array);
+    ?>
+  </p>
+</body>
+</html>
